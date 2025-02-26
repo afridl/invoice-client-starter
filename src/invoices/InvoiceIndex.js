027e30affcafccb90b46ea94611e0d3ad0d02f04
abc <afridlovec@gmail.com>
2025-02-26 20:01:14 +0100

    Removed InvoiceIndex dependency on InvoiceIndexStyle

--- src/invoices/InvoiceIndex.js
+++ src/invoices/InvoiceIndex.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {apiDelete, apiGet} from "../utils/api";
-import "./InvoiceIndexStyle.css";
+
 import InvoiceTable from "./InvoiceTable";
 import InvoiceFilterForm from "./InvoiceFilterForm";
 
